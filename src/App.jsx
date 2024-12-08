79ee32a78f7c70e2c1d77a9221c48967d8ff802a
Subhojit Paul <psubhajit135@gmail.com>
2024-12-08 23:44:27 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import About from "./components/About"
 import Technologies from "./components/Technologies"
 import Education from "./components/Education"
